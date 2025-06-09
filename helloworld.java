cc51fec86c02e0bf35b94d58efbc16b64a23b462
avinash202725 <avinash.k5.dwivedi@gmail.com>
2025-06-09 12:32:04 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, batch39 team with Avinash, Raj");
         System.out.println("Hello, batch39 students are good learner and vikas sir is a good instructor!");
